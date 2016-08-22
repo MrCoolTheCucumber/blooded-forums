bcc472eac07e347037dfbf1ed00ad07775eb4646
Ruben <chickenzrage@gmail.com>
2016-08-22 17:45:48 +0100

    Adds dynamic header logic for when the user is authenticated or not

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+    renderLinks = () => {
+        if (this.props.authenticated) {
+            return (
+                <li className="nav-iten">
+                    <Link to="/signout" className="nav-link">Sign out</Link>
+                </li>
+            );
+        } else {
+            return [
+                <li key={0} className="nav-item">
+                    <Link to="/signin" className="nav-link">Sign in</Link>
+                </li>,
+                <li key={1} className="nav-item">
+                    <Link to="/signup" className="nav-link">Sign up</Link>
+                </li>
+            ];
+        }
+    };
+
     render() {
         return (
-          <nav className="navbar navbar-light">
-            <ul className="nav navbar-nav">
-                <li className="nav-item">Sign in</li>
-            </ul>
-          </nav>
+            <nav className="navbar navbar-light">
+                <Link to="/" className="navbar-brand">Forums</Link>
+                <ul className="nav navbar-nav">
+                    {this.renderLinks()}
+                </ul>
+            </nav>
         );
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return {
+        authenticated: state.auth.authenticated
+    };
+}
+
+export default connect(mapStateToProps)(Header);
