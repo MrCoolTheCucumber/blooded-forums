7f73e300c77564410c97b97d08999b9e83fa7851
Ruben <ostrichboy123@gmail.com>
2016-09-16 00:23:00 +0100

    Adds breadcrumbs action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { reducer as form } from 'redux-form';
 import authReducer from './auth_reducer';
 import forumReducer from './forum_reducer';
 import nanobarReducer from './nanobar_reducer';
+import breadcrumbsReducer from './breadcrumbs_reducer';
 
 const rootReducer = combineReducers({
     form,
     auth: authReducer,
     forum: forumReducer,
-    nanobar: nanobarReducer
+    nanobar: nanobarReducer,
+    breadcrumbs: breadcrumbsReducer
 });
 
 export default rootReducer;
